80f524e230eb382eb3e2536a0704bce1a559eeeb
MaxiSanc37 <maxijsanchez37@gmail.com>
2020-09-21 16:02:38 -0300

    fix post formulario

--- api/methods/formulario/formulario.service.js
+++ api/methods/formulario/formulario.service.js
@@ -3,7 +3,7 @@ const pool = require("../../../config/database");
 module.exports = {
     subirForm: (data, callback) => {
         pool.query(`INSERT INTO formularios (nombre_apellido,nombre_perro,direc,tel,mail,red_social,1_localidad,1_barrio,2_casa_cant,2_edades,3_bebe,4_bebe_x_perro
-                     4_compatibles,4_xq,5_anima_casa,6_tuvieron,6_tiempo/porque,7_xq_interes,8_hogar_animal,9_anti_vecino,10_mudanza,10_huida_pais,11_casa_cerrada,12_cama_perro,
+                     4_compatibles,4_xq,5_anima_casa,6_tuvieron,6_tiempo_porque,7_xq_interes,8_hogar_animal,9_anti_vecino,10_mudanza,10_huida_pais,11_casa_cerrada,12_cama_perro,
                      13_solo_perro,14_vacacion_pero,15_castracion_cachorrito,16_comentario) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`, 
         [
           data.nombre_apellido,
@@ -22,7 +22,7 @@ module.exports = {
           data.xq4,
           data.anima_casa5,
           data.tuvieron6,
-          data.tiempo/porque,
+          data.tiempo_porque,
           data.xq_interes,
           data.hogar_animal8,
           data.anti_vecino9,
@@ -34,16 +34,6 @@ module.exports = {
           data.vacacion_perro14,
           data.castracion_cachorrito15,
           data.cometario16,
-
-
-
-
-
-
-
-          
-
-
         ],
             (error, results, fields) => {
                 if (error) {
@@ -56,6 +46,7 @@ module.exports = {
         );
 
     },
+
     agarrarform: (nombre_apellido, callback) => {
        
         pool.query(`SELECT * FROM formularios WHERE nombre_apellido = ?`, 
