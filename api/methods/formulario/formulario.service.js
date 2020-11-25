098b5e3421a82eda996be70038c5fb69c2efb3ba
MaxiSanc37 <maxijsanchez37@gmail.com>
2020-11-25 17:49:52 -0300

    actualizo form v3

--- api/methods/formulario/formulario.service.js
+++ api/methods/formulario/formulario.service.js
@@ -4,7 +4,7 @@ module.exports = {
     subirForm: (data, callback) => {
         pool.query(`INSERT INTO formulario (nombre_apellido,nombre_perro,direc,tel,mail,1_localidad,1_barrio,2_casa_cant,2_edades,2_de_acuerdo,3_bebe,4_bebe_x_perro,
                      4_compatibles,4_xq,5_anima_casa,6_tuvieron,6_tiempo_porque,7_xq_interes,8_hogar_animal,9_anti_vecino,10_mudanza,10_animales_no,10_huida_pais,11_casa_cerrada,12_cama_perro,
-                     13_solo_perro,14_vacaion_perro,15_castracion_cachorrito,16_comentario) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`, 
+                     13_solo_perro,14_vacaion_perro,15_castracion_cachorrito,16_comentario) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`, 
         [
           data.nombre_apellido,
           data.nombre_perro,
