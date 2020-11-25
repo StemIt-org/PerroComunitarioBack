830ac8e1855f3018fc270c42228396a66922030c
MaxiSanc37 <maxijsanchez37@gmail.com>
2020-11-25 17:43:03 -0300

    actualizo form

--- api/methods/formulario/formulario.service.js
+++ api/methods/formulario/formulario.service.js
@@ -2,20 +2,20 @@ const pool = require("../../../config/database");
 
 module.exports = {
     subirForm: (data, callback) => {
-        pool.query(`INSERT INTO formulario (nombre_apellido,nombre_perro,direc,tel,mail,red_social,1_localidad,1_barrio,2_casa_cant,2_edades,3_bebe,4_bebe_x_perro,
-                     4_compatibles,4_xq,5_anima_casa,6_tuvieron,6_tiempo_porque,7_xq_interes,8_hogar_animal,9_anti_vecino,10_mudanza,10_huida_pais,11_casa_cerrada,12_cama_perro,
-                     13_solo_perro,14_vacaion_perro,15_castracion_cachorrito,16_comentario) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`, 
+        pool.query(`INSERT INTO formulario (nombre_apellido,nombre_perro,direc,tel,mail,1_localidad,1_barrio,2_casa_cant,2_edades,2_de_acuerdo,3_bebe,4_bebe_x_perro,
+                     4_compatibles,4_xq,5_anima_casa,6_tuvieron,6_tiempo_porque,7_xq_interes,8_hogar_animal,9_anti_vecino,10_mudanza,10_animales_no,10_huida_pais,11_casa_cerrada,12_cama_perro,
+                     13_solo_perro,14_vacaion_perro,15_castracion_cachorrito,16_comentario) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`, 
         [
           data.nombre_apellido,
           data.nombre_perro,
           data.direc,
           data.tel,
           data.mail,
-          data.red_social,
           data.localidad1,
           data.barrio1,
           data.casa_cant2,
           data.edades2,
+          data.de_acuerdo2,
           data.bebe3,
           data.bebe_x_perro4,
           data.compatibles4,
@@ -27,6 +27,7 @@ module.exports = {
           data.hogar_animal8,
           data.anti_vecino9,
           data.mudanza10,
+          data.animales_no10,
           data.huida_pais10,
           data.casa_cerrada11,
           data.cama_perro12,
